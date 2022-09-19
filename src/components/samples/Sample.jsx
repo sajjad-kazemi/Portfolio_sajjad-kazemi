d88b3b4a6ff43ad2fc2c4f30be8af057be6d91fc
Sajjad-Kazemi <sajjad9410kazemi@gmail.com>
2022-09-19 23:50:12 +0430

    some small changes

--- src/components/samples/Sample.jsx
+++ src/components/samples/Sample.jsx
@@ -7,7 +7,7 @@ function Sample({ resume }) {
         return (
           <div style={{backgroundImage:`url(${sample.img})`}} className="sample" key={index}>
             <div className="btns">
-              {sample.code && (<a target="_blank" href={sample.code}>Code</a>)}
+              {sample.code && (<a target="_blank" href={sample.code}>Source</a>)}
               {sample.preview && (<a target="_blank" href={sample.preview} >Preview</a>)}
             </div>
             <h1 className="title">{sample.name}</h1>
